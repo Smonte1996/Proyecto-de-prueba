6f6a522b9a412e6117dfdd990e375d1a48fbdd85
Steven Montenegro <smontenegrot@ransa.net>
2022-11-28 14:06:59 -0500

    first comit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//ruta del controlador para el formulario de registro 
+Route::get('/Register',[registrarcontroller::class, 'registar'])->name('Register');
