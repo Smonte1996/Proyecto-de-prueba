4b4c7c2ddb682c934a4eb1b754a25b75dbafa70b
Steven Montenegro <smontenegrot@ransa.net>
2022-12-13 11:24:23 -0500

    AppServiceProvider con https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('production')) 
+        {
+            URL::forceScheme('https');
+        }
     }
 }
